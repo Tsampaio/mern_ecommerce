8d5a03b6d22e2736f51b1bfc61405aa542d8ea5c
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-04 20:30:08 +0100

    User Signup

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 //import routes
@@ -14,6 +17,11 @@ mongoose.connect(process.env.DATABASE, {
   useCreateIndex: true,
   useUnifiedTopology: true
 }).then( () => console.log('DB Connected'));
+
+//middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 //routes middleware
 app.use("/api", userRoutes);
--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,16 @@
-exports.sayHi = (req, res) => {
-  res.json({ message: 'Message from controllers'});
+const User = require('../controllers/user');
+
+exports.signup = (req, res) => {
+  console.log('req.body', req.body);
+  const user = new User(req.body);
+  user.save((err, user) => {
+    if(err) {
+      return res.status(400).json({
+        error: err
+      })
+    }
+    res.json({
+      user
+    })
+  })
 }
