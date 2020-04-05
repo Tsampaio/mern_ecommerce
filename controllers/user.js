7b773532b6a59ddeb9c45f1d55d9d9cd86a6f029
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-05 17:23:14 +0100

    User login and logout

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
-const {errorHandler} = require('../helpers/dbErrorHandler');
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+const {errorHandler} = require('../helpers/dbErrorHandler'); 
 
 // using promise
 exports.signup = (req, res) => {
@@ -18,3 +20,37 @@ exports.signup = (req, res) => {
       });
   });
 };
+
+exports.signin = (req, res) => {
+  //find the user based on email
+  const { email, password } = req.body;
+  User.findOne({email}, (err, user) => {
+    if(err || !user) {
+      return res.status(400).json({
+        error: 'User with that email does not exist. Please Sign up'
+      })
+    } 
+    //if user is found make sure that email and password match
+    //create authenticate method in user model
+    if(!user.authenticate(password)) {
+      return res.status(401).json({
+        error: 'Email and password dont match'
+      })
+    }
+
+    //generate a signed token with user id and secret
+    const token = jwt.sign({ _id: user._id}, process.env.JWT_SECRET);
+
+    //persist the token as 't' in cookie with expiry date
+    res.cookie('t', token, {expire: new Date() + 9999});
+
+    //return response with user and token to frontend client
+    const { _id, name, email, role } = user;
+    return res.json({token, user: { _id, email, name, role}});
+  })
+}
+
+exports.signout = (req, res) => {
+  res.clearCookie('t');
+  res.json({ message: 'Signout success' });
+}
