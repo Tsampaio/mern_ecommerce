e8abb23b4e45fb9ca3ed5e4971182226016a165f
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-05 17:37:42 +0100

    Require login middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,3 +54,8 @@ exports.signout = (req, res) => {
   res.clearCookie('t');
   res.json({ message: 'Signout success' });
 }
+
+exports.requireSignin = expressJwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: 'auth'
+})
