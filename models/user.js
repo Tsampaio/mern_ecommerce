ff38e0a85d45d7ef567fb141b36928fee47d9b1c
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-07 22:16:41 +0100

    Category model and route

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 const uuidv1 = require('uuid/v1');
 
-const userSchema = new mongoose.Schema(
+const categorySchema = new mongoose.Schema(
     {
         name: {
             type: String,
