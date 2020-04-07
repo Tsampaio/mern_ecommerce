c48c08b13a6f0c04f4aa2fa9b61f85670e4b0563
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-07 22:25:51 +0100

    Creating a category with Postman

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const crypto = require('crypto');
 const uuidv1 = require('uuid/v1');
 
-const categorySchema = new mongoose.Schema(
+const userSchema = new mongoose.Schema(
     {
         name: {
             type: String,
