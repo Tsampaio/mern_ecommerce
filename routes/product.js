29f22b3c712a2e3246c10801013da9190a195fc1
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-11 13:15:07 +0100

    List products by search

--- routes/product.js
+++ routes/product.js
@@ -9,7 +9,8 @@ const {
   update, 
   list, 
   listRelated, 
-  listCategories } = require('../controllers/product');
+  listCategories,
+  listBySearch } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
@@ -21,6 +22,7 @@ router.put('/product/:productId/:userId', requireSignin, isAuth, isAdmin, update
 router.get('/products', list);
 router.get('/products/related/:productId', listRelated);
 router.get('/products/categories', listCategories);
+router.post("/products/by/search", listBySearch);
 
 router.param('userId', userById);
 router.param('productId', productById);
