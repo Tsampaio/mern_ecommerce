8a2a60d2db87198f358b46c05372022b609a2dda
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-12 14:13:01 +0100

    Routing Pages

---
+++ frontend/src/Routes.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Signin from "./user/Signin";
+import Signup from "./user/Signup";
+import Home from "./core/Home";
+import Menu from "./core/Menu";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Menu />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signin" component={Signin} />
+        <Route exact path="/signup" component={Signup} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Routes;
