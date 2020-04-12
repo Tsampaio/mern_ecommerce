9f62b24623ac0f5cfc5b3c9bfb5441b8b02ad660
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-12 23:10:06 +0100

    Signup form handle change

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Layout from '../core/Layout';
+import { API } from '../config';
 
 const Signup = () => {
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    password: '',
+    error: '',
+    success: false
+  });
+
+  const handleChange = name => event => {
+    setValues({...values, error: false, [name]: event.target.value});
+  }
+
+  const signUpForm = () => (
+    <form>
+      <div className="from-group">
+        <label className="text-muted">Name</label>
+        <input onChange={handleChange('name')} type="text" className="form-control"/>
+      </div>
+      <div className="from-group">
+        <label className="text-muted">Email</label>
+        <input onChange={handleChange('email')} type="email" className="form-control"/>
+      </div>
+      <div className="from-group">
+        <label className="text-muted">Password</label>
+        <input onChange={handleChange('password')} type="password" className="form-control"/>
+      </div>
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  )
+
   return (
-    <Layout title="Signup" description="Signup to Node React Ecommerce">
-      ...
+    <Layout 
+      title="Signup" 
+      description="Signup to Node React Ecommerce"
+      className="container col-md-8 offset-md-2"
+    >
+      {signUpForm()}
+      {JSON.stringify(values)}
     </Layout>
   )
 }
