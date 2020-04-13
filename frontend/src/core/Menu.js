092d35d9c75292de50d3fadaeb86d31e1a31cbe5
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-13 16:49:54 +0100

    User signout

--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom';
+import { signout } from '../auth';
 
 const isActive = (history, path) => {
   if(history.location.pathname === path) {
@@ -21,6 +22,16 @@ const Menu = ({ history }) => {
         </li>
         <li className="nav-item">
           <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
+        </li>
+        <li className="nav-item">
+          <span 
+            className="nav-link" 
+            style={{cursor: 'pointer', color: '#ffffff'}} 
+            onClick={() => 
+              signout(() => {
+                history.push("/")
+              })
+            }>Signout</span>
         </li>
       </ul>
     </div>
