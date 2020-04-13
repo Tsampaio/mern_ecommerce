d389f1402fc783e753dc330e6fb7f996812eb99f
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-13 17:35:13 +0100

    User Dashboard

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -13,7 +13,7 @@ const Routes = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/signin" component={Signin} />
         <Route exact path="/signup" component={Signup} />
-        <PrivateRoute path="/dashboard" exact component={Dashboard} />
+        <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
       </Switch>
     </BrowserRouter>
   )
--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
-import {signin, authenticate} from '../auth';
+import { signin, authenticate, isAuthenticated } from '../auth';
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -12,7 +12,8 @@ const Signin = () => {
     redirectToReferrer: false
   });
 
-  const { email, password, error, loading, redirectToReferrer } = values
+  const { email, password, error, loading, redirectToReferrer } = values;
+  const {user} = isAuthenticated();
 
   const handleChange = name => event => {
     setValues({...values, error: false, [name]: event.target.value});
@@ -66,7 +67,11 @@ const Signin = () => {
 
   const redirectUser = () => {
     if(redirectToReferrer) {
-      return <Redirect to="/" />
+      if(user && user.role === 1 ) {
+        return <Redirect to="/admin/dashboard" />
+      } else {
+        return <Redirect to="/user/dashboard" />
+      }
     }
   }
 
