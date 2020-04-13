c21aed2b3de19bbb36d20f181f159592b4005ce9
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-13 16:31:15 +0100

    Save user and token into localStorage

--- frontend/src/user/Signin.js
+++ frontend/src/user/Signin.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import Layout from '../core/Layout';
-import {signin} from '../auth';
+import {signin, authenticate} from '../auth';
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -26,10 +26,15 @@ const Signin = () => {
       if(data.error) {
         setValues({...values, error: data.error, loading: false })
       } else {
-        setValues({
-          ...values,
-          redirectToReferrer: true
-        })
+        authenticate(
+          data,
+          () => {
+            setValues({
+              ...values,
+              redirectToReferrer: true
+            });
+          }
+        )
       }
     });
       
@@ -67,8 +72,8 @@ const Signin = () => {
 
   return (
     <Layout 
-      title="Signup" 
-      description="Signup to Node React Ecommerce"
+      title="Sign" 
+      description="Signin to Node React Ecommerce"
       className="container col-md-8 offset-md-2"
     >
       {showLoading()}
