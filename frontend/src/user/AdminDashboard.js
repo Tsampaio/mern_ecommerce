0468747ba0ce70bea9b08582228cb7cc42d4ae63
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-13 17:50:44 +0100

    Admin Dashboard

--- frontend/src/user/AdminDashboard.js
+++ frontend/src/user/AdminDashboard.js
@@ -3,20 +3,20 @@ import Layout from '../core/Layout';
 import {isAuthenticated} from '../auth';
 import { Link } from 'react-router-dom';
 
-const UserDashboard = () => {
+const AdminDashboard = () => {
 
   const {user: {_id, name, email, role}} = isAuthenticated();
 
-  const userLinks = () => {
+  const adminLinks = () => {
     return (
       <div className="card">
-        <h4 className="card-header">User Links</h4>
+        <h4 className="card-header">Admin Links</h4>
         <ul className="list-group">
           <li className="list-group-item">
-            <Link className="nav-link" to="/cart">My Cart</Link>
+            <Link className="nav-link" to="/create/category">Create Category</Link>
           </li>
           <li className="list-group-item">
-            <Link className="nav-link" to="/profile/update">Update Profile</Link>
+            <Link className="nav-link" to="/create/product">Create Product</Link>
           </li>
          
         </ul>
@@ -24,7 +24,7 @@ const UserDashboard = () => {
     )
   }
 
-  const userInfo = () => {
+  const adminInfo = () => {
     return (
       <div className="card mb-5">
         <h3 className="card-header">User Information</h3>
@@ -37,30 +37,18 @@ const UserDashboard = () => {
     );
   };
 
-  const purchaseHistory = () => {
-    return (
-      <div className="card mb-5">
-        <h3 className="card-header">Purchase History</h3>
-        <ul className="list-group">
-          <li className="list-group-item">history</li>
-        </ul>
-      </div>
-    )
-  }
-
   return (
     <Layout title="Dashboard" description={`Good Day ${name}`} className="container-fluid">
       <div className="row">
         <div className="col-3">
-          {userLinks()}
+          {adminLinks()}
         </div>
         <div className="col-9">
-          {userInfo()}
-          {purchaseHistory()}
+          {adminInfo()}
         </div>
       </div>
     </Layout>
   )
 }
 
-export default UserDashboard;
+export default AdminDashboard;
