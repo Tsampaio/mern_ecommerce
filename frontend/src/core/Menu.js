d627cd078a8a47110e3d687543c27f0af24d298d
Telmo Sampaio <telmoasampaio@gmail.com>
2020-04-13 17:01:04 +0100

    Show and Hide signin and signout links conditionally

--- frontend/src/core/Menu.js
+++ frontend/src/core/Menu.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link, withRouter } from 'react-router-dom';
-import { signout } from '../auth';
+import { signout, isAuthenticated } from '../auth';
 
 const isActive = (history, path) => {
   if(history.location.pathname === path) {
@@ -17,22 +17,31 @@ const Menu = ({ history }) => {
         <li className="nav-item">
           <Link className="nav-link" style={isActive(history, '/')} to="/">Home</Link>
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">Signin</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
-        </li>
-        <li className="nav-item">
-          <span 
-            className="nav-link" 
-            style={{cursor: 'pointer', color: '#ffffff'}} 
-            onClick={() => 
-              signout(() => {
-                history.push("/")
-              })
-            }>Signout</span>
-        </li>
+        
+        {!isAuthenticated() && (
+          <React.Fragment>
+            <li className="nav-item">
+              <Link className="nav-link" style={isActive(history, '/signin')} to="/signin">Signin</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" style={isActive(history, '/signup')} to="/signup">Signup</Link>
+            </li>
+          </React.Fragment>
+        )}
+        
+        {isAuthenticated() && (
+          <li className="nav-item">
+            <span 
+              className="nav-link" 
+              style={{cursor: 'pointer', color: '#ffffff'}} 
+              onClick={() => 
+                signout(() => {
+                  history.push("/")
+                })
+              }>Signout</span>
+          </li>
+        )}
+        
       </ul>
     </div>
   )
